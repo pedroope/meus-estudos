ec4e305956d1f3f0b79f499d152db796f93423e9
pedroope <160734834+pedroope@users.noreply.github.com>
2025-07-22 09:09:56 -0300

    Add files via upload

---
+++ exercicios.java
@@ -0,0 +1,53 @@
+package aula04;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class exercicios {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		Aluno[] turma = new Aluno[5];
+		
+		for (int i = 0; i < turma.length; i++) {
+			
+			System.out.println("Escreva Nome do Aluno" + (i+1) + ":" );
+			String nome = scanner.nextLine();
+			
+			System.out.println("Escreva a primeira nota" + (i+1) + ":");
+			double nota1 = scanner.nextDouble();
+			scanner.nextLine();
+			
+			System.out.println("Escreva a segunda nota" + (i+1) + "");
+			double nota2 = scanner.nextDouble();
+			scanner.nextLine();
+			
+			turma[i] = new Aluno(nome, nota1, nota2);	
+			
+			
+		}
+		System.out.println("Média dos alunos");
+		for (Aluno aluno : turma) {
+		 System.out.println("Nome: " + aluno.nome + " | Média: " + aluno.CalcularMedia());
+		}
+		scanner.close();
+		}
+	}
+
+
+class Aluno {
+	String nome;
+	double nota1;
+	double nota2;
+	
+	
+	public Aluno(String nome, double nota1, double nota2) {
+		this.nome = nome;
+		this.nota1 = nota1;
+		this.nota2 = nota2;
+	
+}
+	public double CalcularMedia () {
+		return (nota1 + nota2) /2;
+	}
+	
+}
